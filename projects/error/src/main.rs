7ce16e2cbdb43e8535c00279b1b27eac59077ffe
Nicolas Sauzede <nsauzede@laposte.net>
2020-08-24 23:39:22 +0200

    Finish error exercise

--- projects/error/src/main.rs
+++ projects/error/src/main.rs
@@ -1,7 +1,8 @@
 use std::fs::File;
 use std::io;
 use std::io::Read;
-fn read_username_from_file() -> Result<String, io::Error> {
+use std::fs;
+pub fn read_username_from_file0() -> Result<String, io::Error> {
     let f = File::open("hello.txt");
     let mut f = match f {
         Ok(file) => file,
@@ -12,6 +13,20 @@ fn read_username_from_file() -> Result<String, io::Error> {
         Ok(_) => Ok(s),
         Err(e) => Err(e),
     }
+}
+pub fn read_username_from_file1() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+pub fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+fn read_username_from_file() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
 }
 fn main() {
     let u = read_username_from_file().unwrap();
