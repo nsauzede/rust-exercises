f17d745ba92b6746b7f80a7f5f01dbce3787fba3
Nicolas Sauzede <nsauzede@laposte.net>
2020-08-25 06:19:12 +0200

    Use trait bounds again

--- projects/generics/src/main.rs
+++ projects/generics/src/main.rs
@@ -1,17 +1,29 @@
-fn largest(list: &[i32]) -> i32 {
+pub fn largest0<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
-    for &number in list {
-        if number > largest {
-            largest = number;
+    for &item in list {
+        if item > largest {
+            largest = item;
         }
     }
     largest
 }
+pub fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    &largest
+}
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest(&number_list);
+    let result = largest0(&number_list);
     println!("The largest number is {}", result);
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+    let str_list = vec![String::from("yo"), String::from("zazou"), "zozo".to_string(), "toto".to_string()];
+    let result = largest(&str_list);
+    println!("The largest str is {}", result);
 }
