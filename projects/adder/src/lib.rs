054b0a8074458c8678da017fb3f28c8672352ea6
Nicolas Sauzede <nsauzede@laposte.net>
2020-08-25 14:24:47 +0200

    Add more tests

--- projects/adder/src/lib.rs
+++ projects/adder/src/lib.rs
@@ -1,12 +1,155 @@
+#[derive(Debug, PartialEq)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+//        if value < 1 || value > 100 {
+//        if value < 1 {
+//            panic!("Guess value must be between 1 and 100, got {}.", value);
+//        }
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        }
+
+        Guess { value }
+    }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+//    format!("Hello!")
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
+    }
+
     #[test]
     fn exploration() {
         assert_eq!(2 + 2, 4);
     }
     #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+    #[test]
     #[should_panic]
     fn another() {
         panic!("Make this test panic");
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 6,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+    #[test]
+    fn are_equal() {
+        let r1 = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let r2 = Rectangle {
+            width: 5+3,
+            height: 1+6,
+        };
+
+        assert_eq!(r1, r2);
+    }
+    #[test]
+    fn are_not_equal() {
+        let r1 = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let r2 = Rectangle {
+            width: 3,
+            height: 6,
+        };
+
+        assert_ne!(r1, r2);
+    }
+    #[test]
+    fn larger_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
+    }
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+    #[test]
+    #[should_panic(expected = "Guess value must be greater than or equal to 1")]
+    fn less_than_1() {
+        Guess::new(0);
     }
 }
