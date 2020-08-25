479c8c610d8246f6da34b9f69a2e8236e0d2d4c4
Nicolas Sauzede <nsauzede@laposte.net>
2020-08-25 23:25:25 +0200

    Add some unit tests to minigrep

--- projects/minigrep/src/lib.rs
+++ projects/minigrep/src/lib.rs
@@ -1,9 +1,45 @@
 use std::fs;
 use std::error::Error;
 
+#[derive(Debug, PartialEq)]
 pub struct Config {
     pub query: String,
     pub filename: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn config_ok() {
+        assert_eq!(
+            Config::new(&vec!["progname".to_string(), "needle".to_string(), "haystack".to_string()]).expect("Failed to create Config"),
+            Config{query: "needle".to_string(), filename: "haystack".to_string()}
+        );
+    }
+    #[test]
+    #[should_panic]
+    fn config_fail() {
+        // following should panic because first arg (progname) is missing
+        Config::new(&vec!["needle".to_string(), "haystack".to_string()]).expect("Failed to create Config");
+    }
+    #[test]
+    fn run_ok() {
+        let config = Config::new(&vec!["progname".to_string(), "the".to_string(), "/dev/null".to_string()]).expect("Failed to create Config");
+        // following should pass because /dev/null exists and can be open/read
+        if let Err(e) = run(config) {
+            panic!("Failed to run: {}", e);
+        }
+    }
+    #[test]
+    #[should_panic]
+    fn run_fail() {
+        let config = Config::new(&vec!["progname".to_string(), "the".to_string(), "/dev/notnull".to_string()]).expect("Failed to create Config");
+        // following should panic because /dev/notnull doesn't exist
+        if let Err(e) = run(config) {
+            panic!("Failed to run: {}", e);
+        }
+    }
 }
 
 impl Config {
