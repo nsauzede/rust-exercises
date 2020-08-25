3ed095225910e70308ba7ade3447cb468be66d88
Nicolas Sauzede <nsauzede@laposte.net>
2020-08-25 23:45:26 +0200

    Add TDD search impl

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -9,9 +9,6 @@ fn main() {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
