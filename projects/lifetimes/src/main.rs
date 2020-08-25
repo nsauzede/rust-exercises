15bcd3bc0c8178c55ab51ddf011788537926ecbb
Nicolas Sauzede <nsauzede@laposte.net>
2020-08-25 13:31:02 +0200

    Complete lifetimes exercise

--- projects/lifetimes/src/main.rs
+++ projects/lifetimes/src/main.rs
@@ -5,10 +5,49 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
         y
     }
 }
+
+struct ImportantExcerpt<'a> {
+    _part: &'a str,
+}
+
+use std::fmt::Display;
+
+pub fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn main() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
+
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let _i = ImportantExcerpt {
+        _part: first_sentence,
+    };
+
+    longest_with_an_announcement(&string1[..], string2, "Let's go !!");
 }
