e119522219ad3ce026a52491ff9087de69aa9c75
Nicolas Sauzede <nsauzede@laposte.net>
2020-08-26 16:08:30 +0200

    Add case insensitivity

--- projects/minigrep/src/lib.rs
+++ projects/minigrep/src/lib.rs
@@ -1,10 +1,12 @@
-use std::fs;
+use std::env;
 use std::error::Error;
+use std::fs;
 
 #[derive(Debug, PartialEq)]
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 #[cfg(test)]
@@ -13,19 +15,34 @@ mod tests {
     #[test]
     fn config_ok() {
         assert_eq!(
-            Config::new(&vec!["progname".to_string(), "needle".to_string(), "haystack".to_string()]).expect("Failed to create Config"),
-            Config{query: "needle".to_string(), filename: "haystack".to_string()}
+            Config::new(&vec![
+                "progname".to_string(),
+                "needle".to_string(),
+                "haystack".to_string(),
+            ])
+            .expect("Failed to create Config"),
+            Config {
+                query: "needle".to_string(),
+                filename: "haystack".to_string(),
+                case_sensitive: true,
+            }
         );
     }
     #[test]
     #[should_panic]
     fn config_fail() {
         // following should panic because first arg (progname) is missing
-        Config::new(&vec!["needle".to_string(), "haystack".to_string()]).expect("Failed to create Config");
+        Config::new(&vec!["needle".to_string(), "haystack".to_string()])
+            .expect("Failed to create Config");
     }
     #[test]
     fn run_ok() {
-        let config = Config::new(&vec!["progname".to_string(), "the".to_string(), "/dev/null".to_string()]).expect("Failed to create Config");
+        let config = Config::new(&vec![
+            "progname".to_string(),
+            "the".to_string(),
+            "/dev/null".to_string(),
+        ])
+        .expect("Failed to create Config");
         // following should pass because /dev/null exists and can be open/read
         if let Err(e) = run(config) {
             panic!("Failed to run: {}", e);
@@ -34,21 +51,51 @@ mod tests {
     #[test]
     #[should_panic]
     fn run_fail() {
-        let config = Config::new(&vec!["progname".to_string(), "the".to_string(), "/dev/notnull".to_string()]).expect("Failed to create Config");
+        let config = Config::new(&vec![
+            "progname".to_string(),
+            "the".to_string(),
+            "/dev/notnull".to_string(),
+        ])
+        .expect("Failed to create Config");
         // following should panic because /dev/notnull doesn't exist
         if let Err(e) = run(config) {
             panic!("Failed to run: {}", e);
         }
     }
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -68,13 +115,23 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+    for line in results {
         println!("{}", line);
     }
     Ok(())
