cbf4dc85eba93498796b7af940b3fefd8b0fd38b
Nicolas Sauzede <nsauzede@laposte.net>
2020-08-28 06:12:42 +0200

    More boxes code

--- projects/boxes/src/main.rs
+++ projects/boxes/src/main.rs
@@ -1,7 +1,46 @@
+use crate::List::{Cons, Nil};
+use std::ops::Deref;
+
 fn type_of<T>(_: &T) -> String {
     format!("{}", std::any::type_name::<T>())
 }
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+#[derive(Debug)]
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
+
 fn main() {
     let b = Box::new(0b_1010_0101);
-    println!("*b = {:#b} ({})", *b, type_of(&*b));
+    println!("*b = {:?} ({})", *b, type_of(&*b));
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("l = {:?} ({})", list, type_of(&list));
+
+    let mb = MyBox::new(*b);
+    println!("*mb = {:?} ({})", *mb, type_of(&*mb));
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
 }
