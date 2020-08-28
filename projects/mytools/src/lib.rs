e74fc4bf7aefdbeb9a805c3eed2604bf3a3118bb
Nicolas Sauzede <nsauzede@laposte.net>
2020-08-28 10:39:33 +0200

    Polish doc

--- projects/mytools/src/lib.rs
+++ projects/mytools/src/lib.rs
@@ -10,26 +10,23 @@
 /// let convoluted = &Box::new((vec![123f32],3.14.to_string()));
 /// let type_name = mytools::type_of(&convoluted);
 /// println!("{}", type_name);
-/// assert_eq!("&alloc::boxed::Box<(alloc::vec::Vec<f32>, alloc::string::String)>", type_name);
+/// assert_eq!(
+///     "&alloc::boxed::Box<(alloc::vec::Vec<f32>, alloc::string::String)>",
+///     type_name
+/// );
 /// ```
 ///
 /// # Examples
 /// Examples below ensure that the type name got is as expected.
 /// ```
 /// let forty_two = 42; // i32
-/// let pi = 3.14;      // f64
 /// assert_eq!("i32", mytools::type_of(&forty_two));
+/// let pi = 3.14;      // f64
 /// assert_eq!("f64", mytools::type_of(&pi));
-/// ```
-/// ```
-/// let boxed_notb = Box::new(666);
-/// assert_eq!("alloc::boxed::Box<i32>", mytools::type_of(&boxed_notb));
-/// ```
-/// ```
+/// let notb = Box::new(666);
+/// assert_eq!("alloc::boxed::Box<i32>", mytools::type_of(&notb));
 /// let bigint = 1i64 << 33;
 /// assert_eq!("i64", mytools::type_of(&bigint));
-/// ```
-/// ```
 /// let var = "string litteral";
 /// assert_eq!("&str", mytools::type_of(&var));
 /// ```
